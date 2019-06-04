4e20e22aa8b73ee105f2aa60a7c74546295fe410
s265485 <s265485@studenti.polito.it>
2019-06-04 10:48:35 +0200

    Changed Beverage class in order to fix #11

--- src/main/java/it/polito/latazza/data/Beverage.java
+++ src/main/java/it/polito/latazza/data/Beverage.java
@@ -1,6 +1,8 @@
 package it.polito.latazza.data;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import it.polito.latazza.exceptions.BeverageException;
 import it.polito.latazza.exceptions.NotEnoughCapsules;
@@ -9,18 +11,21 @@ public class Beverage implements Serializable {
 	private static final long serialVersionUID = 3420502585988933185L;
 	
 	// all prices are expressed in cents
-	private int id, boxPrice, capsulesPerBox, availableCapsules;
+	private int id;
+	private List<BeverageData> beverageData;
 	private String name;
+	
 	
 	public Beverage(Integer id, String name, Integer boxPrice , Integer capsulePerBox) throws BeverageException {
 		if (id < 0)
 			throw new BeverageException();
 		this.id = id;
 		
-		this.availableCapsules = 0;
+		this.beverageData = new ArrayList<Beverage.BeverageData>();
+		this.beverageData.add(new BeverageData());
 		setName(name);
 		setCapsulesPerBox(capsulePerBox);
-		setBoxPrice(boxPrice);		
+		setBoxPrice(boxPrice);
 	}
 
 	public int getId() {
@@ -38,54 +43,129 @@ public class Beverage implements Serializable {
 	}
 	
 	public int getBoxPrice() {
-		return boxPrice;
+		BeverageData bd = this.beverageData.get(this.beverageData.size()-1); //take last updated beverage data
+		return bd.getBoxPrice();
 	}
 	
-	public void setBoxPrice(Integer boxPrice) throws BeverageException {
+	private void setBoxPrice(Integer boxPrice) throws BeverageException {
+		BeverageData bd = this.beverageData.get(this.beverageData.size()-1); //take last updated beverage data
 		if(boxPrice == null || boxPrice < 0)
 			throw new BeverageException();
-		this.boxPrice = boxPrice;
+		bd.setBoxPrice(boxPrice);
 	}
 	
 	public int getCapsulesPerBox() {
-		return capsulesPerBox;
+		BeverageData bd = this.beverageData.get(this.beverageData.size()-1); //take last updated beverage data
+		return bd.getCapsulesPerBox();
 	}
 	
-	public void setCapsulesPerBox(Integer capsulesPerBox) throws BeverageException {
+	private void setCapsulesPerBox(Integer capsulesPerBox) throws BeverageException {
+		BeverageData bd = this.beverageData.get(this.beverageData.size()-1); //take last updated beverage data
 		if(capsulesPerBox == null || capsulesPerBox < 0)
 			throw new BeverageException();
-		this.capsulesPerBox = capsulesPerBox;
+		bd.setCapsulesPerBox(capsulesPerBox);
+	}
+	
+	public void updateBeverageData(String name, Integer boxPrice , Integer capsulePerBox) throws BeverageException {
+		this.beverageData.add(new BeverageData());
+		this.setName(name);
+		this.setBoxPrice(boxPrice);
+		this.setCapsulesPerBox(capsulePerBox);
 	}
 	
 	public int getAvailableCapsules() {
-		return availableCapsules;
+		BeverageData bd = this.beverageData.get(this.beverageData.size()-1); //take last updated beverage data
+		return bd.getAvailableCapsules();
 	}
 	
 	public int getCapsulesPrice() {
-		return boxPrice / capsulesPerBox;
+		BeverageData bd = this.beverageData.get(this.beverageData.size()-1); //take last updated beverage data
+		return bd.getBoxPrice() / bd.getCapsulesPerBox();
 	}
 	
 	public void increaseAvailableCapsules(int numberOfBoxes) throws BeverageException {
 		// handle negative value and overflow
-		if (numberOfBoxes < 0 || this.availableCapsules + numberOfBoxes * this.capsulesPerBox < 0)
+		BeverageData bd = this.beverageData.get(this.beverageData.size()-1); //take last updated beverage data
+		if (numberOfBoxes < 0 || bd.getAvailableCapsules() + numberOfBoxes * bd.capsulesPerBox < 0)
 			throw new BeverageException();
 		
-		this.availableCapsules += numberOfBoxes * this.capsulesPerBox;
+		bd.setAvailableCapsules(bd.getAvailableCapsules() + (numberOfBoxes * bd.getCapsulesPerBox()));
 	}
 	
 	public void decreaseAvailableCapsules(int numberOfCapsules) throws NotEnoughCapsules, BeverageException {
-		if (this.availableCapsules < numberOfCapsules )
+		BeverageData bd = this.beverageData.get(0); //take remaining beverage data
+		Integer totalAvailable = this.beverageData.stream().collect(java.util.stream.Collectors.summingInt(l -> l.getAvailableCapsules()));
+		if (totalAvailable < numberOfCapsules )
 			throw new NotEnoughCapsules();
 		
 		// handle negative value and overflow
-		if (numberOfCapsules < 0 || this.availableCapsules - numberOfCapsules < 0)
+		if (numberOfCapsules < 0 || totalAvailable - numberOfCapsules < 0)
 			throw new BeverageException();
 				
-		this.availableCapsules -= numberOfCapsules;
+		while(numberOfCapsules > 0) {
+			// take min between number of capsules to decrease or avaialbe capsules with not updated price
+			int min = (bd.getAvailableCapsules() > numberOfCapsules) ? numberOfCapsules : bd.getAvailableCapsules();
+			//remove the min number of capsules
+			bd.setAvailableCapsules(bd.getAvailableCapsules() - min);
+			//update the amount of capsules to sell
+			numberOfCapsules -= min;
+			//if still capsuels to sell => finished not updated capsules => remove them
+			if(numberOfCapsules > 0)
+				this.beverageData.remove(0);
+			//update the capsules to sell
+			bd = this.beverageData.get(0);
+		}
 	}
 
 	@Override
 	public String toString() {
 		return this.name;
 	}
+	
+	
+	
+	
+	
+	private class BeverageData {
+		private Integer boxPrice,capsulesPerBox, availableCapsules;
+		
+		public BeverageData(Integer boxPrice, Integer capsulesPerBox, Integer availableCapsules) {
+			this.boxPrice=boxPrice;
+			this.capsulesPerBox=capsulesPerBox;
+			this.availableCapsules=availableCapsules;
+		}
+		
+		public BeverageData() {
+			this.boxPrice=0;
+			this.capsulesPerBox=0;
+			this.availableCapsules=0;
+		}
+		
+		public Integer getAvailableCapsules() {
+			return availableCapsules;
+		}
+		
+		public Integer getBoxPrice() {
+			return boxPrice;
+		}
+		
+		public Integer getCapsulesPerBox() {
+			return capsulesPerBox;
+		}
+		
+		public void setAvailableCapsules(Integer availableCapsules) {
+			this.availableCapsules = availableCapsules;
+		}
+		
+		public void setCapsulesPerBox(Integer capsulesPerBox) {
+			this.capsulesPerBox = capsulesPerBox;
+		}
+		
+		public void setBoxPrice(Integer boxPrice) {
+			this.boxPrice = boxPrice;
+		}
+	}
+	
+	
+	
 }
