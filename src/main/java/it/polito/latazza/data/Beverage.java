9de4c12f0d6e6b7206beb42655b9e5b60b76c588
Enrico Loparco <s261072@studenti.polito.it>
2019-06-05 18:49:03 +0200

    Refactor 'Beverage' class to use LinkedList instead of ArrayList

--- src/main/java/it/polito/latazza/data/Beverage.java
+++ src/main/java/it/polito/latazza/data/Beverage.java
@@ -1,8 +1,8 @@
 package it.polito.latazza.data;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedList;
+import java.util.stream.Collectors;
 
 import it.polito.latazza.exceptions.BeverageException;
 import it.polito.latazza.exceptions.NotEnoughCapsules;
@@ -12,17 +12,16 @@ public class Beverage implements Serializable {
 	
 	// all prices are expressed in cents
 	private int id;
-	private List<BeverageData> beverageData;
+	private LinkedList<BeverageData> updatesHistory;
 	private String name;
-	
 	
 	public Beverage(Integer id, String name, Integer boxPrice , Integer capsulePerBox) throws BeverageException {
 		if (id < 0)
 			throw new BeverageException();
 		this.id = id;
 		
-		this.beverageData = new ArrayList<Beverage.BeverageData>();
-		this.beverageData.add(new BeverageData());
+		this.updatesHistory = new LinkedList<Beverage.BeverageData>();
+		this.updatesHistory.add(new BeverageData());
 		setName(name);
 		setCapsulesPerBox(capsulePerBox);
 		setBoxPrice(boxPrice);
@@ -43,31 +42,31 @@ public class Beverage implements Serializable {
 	}
 	
 	public int getBoxPrice() {
-		BeverageData bd = this.beverageData.get(this.beverageData.size()-1); //take last updated beverage data
+		BeverageData bd = this.updatesHistory.getLast(); //take last updated beverage data
 		return bd.getBoxPrice();
 	}
 	
 	private void setBoxPrice(Integer boxPrice) throws BeverageException {
-		BeverageData bd = this.beverageData.get(this.beverageData.size()-1); //take last updated beverage data
+		BeverageData bd = this.updatesHistory.getLast(); //take last updated beverage data
 		if(boxPrice == null || boxPrice < 0)
 			throw new BeverageException();
 		bd.setBoxPrice(boxPrice);
 	}
 	
 	public int getCapsulesPerBox() {
-		BeverageData bd = this.beverageData.get(this.beverageData.size()-1); //take last updated beverage data
+		BeverageData bd = this.updatesHistory.getLast(); //take last updated beverage data
 		return bd.getCapsulesPerBox();
 	}
 	
 	private void setCapsulesPerBox(Integer capsulesPerBox) throws BeverageException {
-		BeverageData bd = this.beverageData.get(this.beverageData.size()-1); //take last updated beverage data
+		BeverageData bd = this.updatesHistory.getLast(); //take last updated beverage data
 		if(capsulesPerBox == null || capsulesPerBox < 0)
 			throw new BeverageException();
 		bd.setCapsulesPerBox(capsulesPerBox);
 	}
 	
 	public void updateBeverageData(String name, Integer boxPrice , Integer capsulePerBox) throws BeverageException {
-		this.beverageData.add(new BeverageData());
+		this.updatesHistory.addLast(new BeverageData());
 			
 		String n = this.getName();
 		
@@ -77,23 +76,23 @@ public class Beverage implements Serializable {
 			this.setCapsulesPerBox(capsulePerBox);
 		} catch (Exception e) {
 			this.setName(n);
-			this.beverageData.remove(this.beverageData.size()-1);
+			this.updatesHistory.removeLast();
 			throw e;
 		}
 	}
 	
 	public int getAvailableCapsules() {
-		return this.beverageData.stream().collect(java.util.stream.Collectors.summingInt(l -> l.getAvailableCapsules()));
+		return this.updatesHistory.stream().collect(Collectors.summingInt(l -> l.getAvailableCapsules()));
 	}
 	
 	public int getCapsulesPrice() {
-		BeverageData bd = this.beverageData.get(this.beverageData.size()-1); //take last updated beverage data
+		BeverageData bd = this.updatesHistory.getLast(); //take last updated beverage data
 		return bd.getBoxPrice() / bd.getCapsulesPerBox();
 	}
 	
 	public void increaseAvailableCapsules(int numberOfBoxes) throws BeverageException {
 		// handle negative value and overflow
-		BeverageData bd = this.beverageData.get(this.beverageData.size()-1); //take last updated beverage data
+		BeverageData bd = this.updatesHistory.getLast(); //take last updated beverage data
 		if (numberOfBoxes < 0 || bd.getAvailableCapsules() + numberOfBoxes * bd.capsulesPerBox < 0)
 			throw new BeverageException();
 		
@@ -101,31 +100,32 @@ public class Beverage implements Serializable {
 	}
 	
 	public int decreaseAvailableCapsules(int numberOfCapsules) throws NotEnoughCapsules, BeverageException {
-		BeverageData bd = this.beverageData.get(0); //take remaining beverage data
+		BeverageData bd = this.updatesHistory.getFirst(); //take remaining beverage data
 		Integer totalAvailable = this.getAvailableCapsules();
+		
 		if (totalAvailable < numberOfCapsules )
 			throw new NotEnoughCapsules();
 		
 		// handle negative value and overflow
 		if (numberOfCapsules < 0 || totalAvailable - numberOfCapsules < 0)
 			throw new BeverageException();
-		int amnt = 0;
-		while(numberOfCapsules > 0) {
-			// take min between number of capsules to decrease or avaialbe capsules with not updated price
-			int min = (bd.getAvailableCapsules() > numberOfCapsules) ? numberOfCapsules : bd.getAvailableCapsules();
-			//remove the min number of capsules
-			amnt += min * bd.getBoxPrice() / bd.getCapsulesPerBox();
-			bd.setAvailableCapsules(bd.getAvailableCapsules() - min);
-			//update the amount of capsules to sell
-			numberOfCapsules -= min;
-			//if still capsuels to sell => finished not updated capsules => remove them
-			if(numberOfCapsules > 0)
-				this.beverageData.remove(0);
-			//update the capsules to sell
-			bd = this.beverageData.get(0);
-		}
-		return amnt;
 		
+		int amount = 0;
+		while (numberOfCapsules > 0) {
+			// take min between number of capsules requested and number of capsules available from the least recent update
+			int min = Math.min(bd.getAvailableCapsules(), numberOfCapsules);
+			// remove the min number of capsules
+			amount += min * bd.getBoxPrice() / bd.getCapsulesPerBox();
+			bd.setAvailableCapsules(bd.getAvailableCapsules() - min);
+			// update the amount of capsules to sell
+			numberOfCapsules -= min;
+			// if still capsules to sell => finished not updated capsules => remove them
+			if (numberOfCapsules > 0)
+				this.updatesHistory.removeFirst();
+			// use capsules from the new least recent update
+			bd = this.updatesHistory.getFirst();
+		}
+		return amount;
 	}
 
 	@Override
@@ -133,17 +133,14 @@ public class Beverage implements Serializable {
 		return this.name;
 	}
 	
-	
-	
-	
-	
 	private class BeverageData implements Serializable {
+		private static final long serialVersionUID = 1L;
 		private Integer boxPrice,capsulesPerBox, availableCapsules;
 		
 		public BeverageData() {
-			this.boxPrice=0;
-			this.capsulesPerBox=0;
-			this.availableCapsules=0;
+			this.boxPrice = 0;
+			this.capsulesPerBox = 0;
+			this.availableCapsules = 0;
 		}
 		
 		public Integer getAvailableCapsules() {
@@ -169,8 +166,5 @@ public class Beverage implements Serializable {
 		public void setBoxPrice(Integer boxPrice) {
 			this.boxPrice = boxPrice;
 		}
-	}
-	
-	
-	
+	}	
 }
